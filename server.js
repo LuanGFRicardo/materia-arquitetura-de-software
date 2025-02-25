fead14854cf81f2ea55b98cbf3ba67dff1923afd
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2025-02-24 21:42:15 -0300

    Commit 2.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
 const rotas = {
     "/": "API com Node e Express.js",
+    "/posts": "Rota de postagem",
+    "/autores": "Rota de autores",
 }
 
-const server = http.createServer((req, res) => {
-    res.writedHead(200, {"Content-Type": "text/plain" });
-});
+//const server = http.createServer((req, res) => {
+//    res.writeHead(200, {"Content-Type": "text/plain" });
+//});
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Servidor na escuta!");
 });
