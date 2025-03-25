e748e679572e0294e09ffa40830266807b020eea
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2025-03-24 21:59:43 -0300

    Aula dia 24/03/2025

--- aula-4/src/controllers/authorController.js
+++ aula-4/src/controllers/authorController.js
@@ -1,9 +1,9 @@
-import post from "../models/Post.js";
+import author from "../models/Author.js";
 
 class AuthorController {
     static async getAllPosts(req, res) {
         try {
-            const listPosts = await post.find({});
+            const listPosts = await author.find({});
             res.status(200).json(listPosts);
         } catch(error) {
             res.status(500).send(error.message);
@@ -12,7 +12,7 @@ class AuthorController {
 
     static async createPost(req, res) {
         try {
-            const newPost = new post(req.body);
+            const newPost = new author(req.body);
             await newPost.save();
             res.status(201).json({
                 message: "Post criado com sucesso!",
@@ -26,7 +26,7 @@ class AuthorController {
     static async getPostById(req, res) {
         try {
             const id = req.params.id;
-            const foundPost = await post.findById(id);
+            const foundPost = await author.findById(id);
             if(!foundPost) {
                 return res.status(404).send("Post não encotrando!");
             }
@@ -39,7 +39,7 @@ class AuthorController {
     static async deletePostById(req, res) {
         try {
             const id = req.params.id;
-            const deletePost = await post.findById(id);
+            const deletePost = await author.findById(id);
             res.status(200).send("Post removido com sucesso!");
             if(!deletePost) {
                 return res.status(404).send("Post não encotrando!");
